1b2662241b79453f4a8b5d25c74f0a585b78a6ef
Shivukumar96 <56018006+Shivukumar96@users.noreply.github.com>
2019-10-01 14:18:27 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,17 @@
+package jen1;
+
+import java.util.Scanner;
+
+public class Add {
+
+	public static void main(String[] args)
+	{
+	 Scanner s = new Scanner(System.in);
+	 int a=s.nextInt();
+	 int b=s.nextInt();
+	 int sum=a+b;
+	 System.out.println(sum);
+
+	}
+
+}
