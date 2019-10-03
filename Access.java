066798c4e2a18579e22702547aa46d7aecc6d408
Shivukumar96 <56018006+Shivukumar96@users.noreply.github.com>
2019-10-03 21:23:47 +0530

    Add files via upload
    My programming is uploaded

---
+++ Access.java
@@ -0,0 +1,19 @@
+public class Access
+{
+	static  int a;
+         static  int b;
+
+public static void main(String args[])
+{
+	
+
+ 	  a=Integer.parseInt(args[0]);
+          b=Integer.parseInt(args[1]);
+          System.out.println(a+b);
+	  
+    
+
+}
+
+}
+  
