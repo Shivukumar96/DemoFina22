56e8b5df0264ae72befea12b568154d432ad2c15
Shivukumar96 <56018006+Shivukumar96@users.noreply.github.com>
2019-10-03 12:22:11 +0530

    Add files via upload
    Programss

---
+++ Program1.java
@@ -0,0 +1,16 @@
+
+public class Program1 {
+
+	public static void main(String[] args) 
+	{
+		int a=10;
+		int n=20;
+		for (int i = 0; i < n; i++) 
+		{
+			System.out.println(a+i);
+			
+		}
+
+	}
+
+}
